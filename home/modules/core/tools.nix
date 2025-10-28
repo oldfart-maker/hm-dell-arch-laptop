a741b8bad9b63153473fb84cf1ac2cbcf5ab160a
Mike Burns <mkburns61@yahoo.com>
2025-10-28 06:13:23 -0400

    fixed migration bugs

--- home/modules/core/tools.nix
+++ home/modules/core/tools.nix
@@ -1,5 +1,5 @@
 # home/modules/tools.nix
-{ config, repoPath ... }:
+{ config, repoPath, ... }:
 {
   # Ensure ~/.local/bin exists and script is linked, executable
   home.file.".local/bin/hm-update" = {
