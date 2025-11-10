bd1b96896f805ed511ddbd41ff667a7e9a4d5120
Mike Burns <mkburns61@yahoo.com>
2025-11-10 08:33:10 -0500

    tangle-sync: update generated configs

--- home/data/apps/emacs/modules/dev.el
+++ home/data/apps/emacs/modules/dev.el
@@ -34,15 +34,45 @@
 (add-hook 'python-mode-hook #'eglot-ensure)
 
 (use-package magit
+  ;; Don’t load Magit at startup; load on demand.
+  :commands (magit-status)
+  :bind
+  ("M-g" . magit-status)
+  :init
+  ;; Work around Magit's fragile custom-init behavior on this setup.
+  (with-eval-after-load 'magit-autoloads
+    (defun magit-custom-initialize-after-init (symbol value)
+      "Robust version of `magit-custom-initialize-after-init'."
+      (internal--define-uninitialized-variable symbol)
+      (cond
+       ;; Not yet after init: defer to `after-init-hook`.
+       ((not after-init-time)
+        (let ((sym symbol)
+              (val value))
+          (letrec ((f (lambda ()
+                        (ignore-errors (remove-hook 'after-init-hook f))
+                        (custom-initialize-set sym val))))
+            (add-hook 'after-init-hook f))))
+       ;; No `load-file-name` – initialize immediately.
+       ((not load-file-name)
+        (custom-initialize-set symbol value))
+       ;; Otherwise, wait until the file that defined the variable is loaded.
+       (t
+        (let* ((thisfile load-file-name)
+               (sym symbol)
+               (val value))
+          (letrec ((f (lambda (file)
+                        (when (equal file thisfile)
+                          (ignore-errors (remove-hook 'after-load-functions f))
+                          (custom-initialize-set sym val)))))
+            (add-hook 'after-load-functions f)))))))
   :config
   (setq magit-push-always-verify nil)
-  (setq git-commit-summary-max-length 50)
-  :bind
-  ("M-g" . magit-status))
+  (setq git-commit-summary-max-length 50))
 
 (use-package treemacs-magit
-  :after treemacs magit)
+  :after (treemacs magit))
 
+;; Let Magit pull ghub in as needed; don’t force it at init.
 (use-package ghub
-  :demand t
-  :after magit)
+  :defer t)
