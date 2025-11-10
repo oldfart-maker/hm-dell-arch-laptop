1a0af6bce7e747f08fbb808a321d05d1ff02bf37
Mike Burns <mkburns61@yahoo.com>
2025-11-10 17:57:24 -0500

    added qutebrowser

---
+++ home/modules/apps/qutebrowser.nix
@@ -0,0 +1,43 @@
+{ pkgs, ... }:
+
+{
+  programs.qutebrowser = {
+    enable = true;
+    settings = {
+      # Default zoom level
+      zoom.default = "110%";
+
+      # Block ads/tracking (requires qtwebengine compiled with adblock)
+      content.blocking.enabled = true;
+
+      # Dark mode if desired
+      colors.webpage.darkmode.enabled = true;
+
+      # Load last session automatically
+      auto_save.session = true;
+      session.default_name = "default";
+
+      # Set search engine
+      url.default_page = "https://duckduckgo.com/";
+      url.searchengines = {
+        DEFAULT = "https://duckduckgo.com/?q={}";
+        g = "https://google.com/search?q={}";
+      };
+
+      # Downloads
+      downloads.location.directory = "~/Downloads";
+    };
+
+    # Optional keybindings
+    keyBindings = {
+      normal = {
+        "d" = "scroll-page 0 0.5";   # scroll down half page
+        "u" = "scroll-page 0 -0.5";  # scroll up half page
+        "J" = "tab-next";
+        "K" = "tab-prev";
+        "x" = "tab-close";
+        "X" = "undo";
+      };
+    };
+  };
+}
