b2c0b97b56f9ec9067c41423b71d986a54ba80b4
Mike Burns <mkburns61@yahoo.com>
2025-11-12 09:25:59 -0500

    remote unused tools.nix

--- home/modules/tools/tools.nix
+++
@@ -1,9 +0,0 @@
-# home/modules/tools/tools.nix
-{ config, repoPath, ... }:
-{
-  # Ensure ~/.local/bin exists and script is linked, executable
-  home.file.".local/bin/hm-update" = {
-    source = repoPath "home/scripts/hm-update.sh";
-    executable = true;
-  };
-}
