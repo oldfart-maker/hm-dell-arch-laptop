17b5da9296b35d2214e01270abb3695b50c9cf49
Mike Burns <mkburns61@yahoo.com>
2025-11-13 19:24:08 -0500

    reverted back to no wrapper

--- home/modules/apps/bitwarden.nix
+++ home/modules/apps/bitwarden.nix
@@ -1,17 +1,5 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
-let
-  bitwardenWrapped = pkgs.writeShellScriptBin "bitwarden" ''
-    # Force Qt to use Wayland, not X11
-    export QT_QPA_PLATFORM=wayland
-    export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
-
-    # Run through nixGL so it sees the host GPU drivers
-    exec nixGL ${pkgs.bitwarden}/bin/bitwarden "$@"
-  '';
-in
 {
-  home.packages = [
-    bitwardenWrapped
-  ];
+  home.packages = [ pkgs.bitwarden ];
 }
