f1a75de49559c35331ba90a1c5be84be6459d2a5
Mike Burns <mkburns61@yahoo.com>
2025-11-13 19:06:20 -0500

    add nixGL wrapper

--- home/modules/apps/bitwarden.nix
+++ home/modules/apps/bitwarden.nix
@@ -1,5 +1,18 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
-{
-  home.packages = [ pkgs.bitwarden ];
+let
+  _Wrapped = pkgs.writeShellScriptBin "bitwarden" ''
+    # Force Qt to use Wayland, not X11
+    export QT_QPA_PLATFORM=wayland
+    export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+
+    # Run through nixGL so it sees the host GPU drivers
+    exec nixGL ${pkgs.bitwarden}/bin/bitwarden "$@"
+  '';
+
+in {  
+  programs.bitwarden = {
+    enable = true;
+    package=_Wrapped
+  };
 }
