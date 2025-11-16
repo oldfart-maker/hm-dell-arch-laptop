ceb1b9c8190ad2be4695eac516e4eead4799085a
Mike Burns <mkburns61@yahoo.com>
2025-11-16 06:05:22 -0500

    remove stupid comments generated by ai

--- home/modules/core/rclone.nix
+++ home/modules/core/rclone.nix
@@ -7,9 +7,6 @@ in
 {
   home.packages = [ pkgs.rclone ];
 
-  # Let HM manage the directory structure if you like, or just rely on the activation script.
-  # The important part is the activation step below.
-
   home.activation.linkRcloneSecrets = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     mkdir -p "${config.home.homeDirectory}/.config/rclone"
 
