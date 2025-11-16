9d3ca0488443452553c8940225bb5795fbbdafcc
Mike Burns <mkburns61@yahoo.com>
2025-11-16 06:01:52 -0500

    removed home.file trying to write rclone.config to hm store during clone

--- home/modules/core/rclone.nix
+++ home/modules/core/rclone.nix
@@ -3,14 +3,20 @@
 
 let
   secretsRoot = "${config.home.homeDirectory}/projects/sys-secrets";
-  mkSecret    = config.lib.file.mkOutOfStoreSymlink;  
 in
 {
   home.packages = [ pkgs.rclone ];
 
-  home.file.".config/rclone/rclone.conf".source =
-    mkSecret "${secretsRoot}/rclone/rclone.conf";
+  # Let HM manage the directory structure if you like, or just rely on the activation script.
+  # The important part is the activation step below.
 
-  home.file.".config/rclone/officesync.filter".source =
-    mkSecret "${secretsRoot}/rclone/officesync.filter";    
+  home.activation.linkRcloneSecrets = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    mkdir -p "${config.home.homeDirectory}/.config/rclone"
+
+    ln -sf "${secretsRoot}/rclone/rclone.conf" \
+      "${config.home.homeDirectory}/.config/rclone/rclone.conf"
+
+    ln -sf "${secretsRoot}/rclone/officesync.filter" \
+      "${config.home.homeDirectory}/.config/rclone/officesync.filter"
+  '';
 }
