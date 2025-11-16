13c4a0a5eddf5725a4fbcad7e50e099ebe85e9fc
Mike Burns <mkburns61@yahoo.com>
2025-11-16 17:19:57 -0500

    wrapped wezterm with nixGL

--- home/modules/core/wezterm.nix
+++ home/modules/core/wezterm.nix
@@ -3,9 +3,21 @@
 let
   cfgPath  = repoPath "home/data/apps/wezterm/wezterm.lua";
   colorsPath = repoPath "home/data/apps/wezterm/colors";
+
+  # Wrap wezterm to use nixGL
+  weztermWrapped = pkgs.writeShellScriptBin "wezterm" ''
+    export WAYLAND_DISPLAY="wayland-0"
+    export WINIT_UNIX_BACKEND=wayland
+
+    # If you have a specific nixGL variant, use it here
+    exec nixGL "${pkgs.wezterm}/bin/wezterm" "$@"
+  '';  
 in
 {
-  home.packages = [ pkgs.wezterm ];
+  programs.wezterm = {
+    enable = true;
+    package = weztermWrapped;
+  }
 
   xdg.configFile."wezterm/wezterm.lua" = {
     source = cfgPath;
