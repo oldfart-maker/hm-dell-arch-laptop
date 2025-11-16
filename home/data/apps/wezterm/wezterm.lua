16095bbbf0c1c7379ffd96202c4e2874ac291183
Mike Burns <mkburns61@yahoo.com>
2025-11-16 17:24:08 -0500

    removed path to fish

--- home/data/apps/wezterm/wezterm.lua
+++ home/data/apps/wezterm/wezterm.lua
@@ -6,7 +6,7 @@ local config = wezterm.config_builder()
 config.initial_cols = 120
 config.initial_rows = 28
 config.font_size = 9
-config.default_prog = {'/usr/bin/fish', '-l'}
+config.default_prog = {'fish', '-l'}
 
 -- Font settings
 -- config.font = wezterm.font_with_fallback({
